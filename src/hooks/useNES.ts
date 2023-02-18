d2a0ae8f85441390602443cf99a921b75ffb39b9
Jerry Wang <x.jerry.wang@gmail.com>
2023-02-18 18:05:23 +0800

    controller

--- src/hooks/useNES.ts
+++ src/hooks/useNES.ts
@@ -1,17 +1,17 @@
-import jsnes from 'jsnes'
-
-const { NES, Controller } = jsnes
+import { NES, Controller } from 'jsnes'
 
 const size = {
   w: 256,
   h: 240,
 }
 
+var SAMPLE_COUNT = 4 * 1024
+var SAMPLE_MASK = SAMPLE_COUNT - 1
+
 export function useNES() {
-  const _canvas = document.createElement('canvas')
-  _canvas.width = size.w
-  _canvas.height = size.h
-  const ctx2d = _canvas.getContext('2d')!
+  const _canvas = createCanvas()
+  const _audio = createAudioContext()
+  const { ctx2d } = _canvas
 
   const ctx = reactive({
     paused: false,
@@ -27,29 +27,29 @@ export function useNES() {
 
   // Initialize and set up outputs
   const nes = new NES({
-    onFrame: function (buffer: any) {
-      var i = 0
-      for (var y = 0; y < size.h; ++y) {
-        for (var x = 0; x < size.w; ++x) {
-          i = y * size.w + x
+    onFrame: (buffer: number[]) => {
+      for (let y = 0; y < size.h; ++y) {
+        for (let x = 0; x < size.w; ++x) {
+          const i = y * size.w + x
           // Convert pixel from NES BGR to canvas ABGR
           output.buf32[i] = 0xff000000 | buffer[i] // Full alpha
         }
       }
     },
     onStatusUpdate: console.log,
+    sampleRate: _audio.ctx.sampleRate,
     onAudioSample: function (l: any, r: any) {
-      // console.log(l, r)
-      // audio_samples_L[audio_write_cursor] = l
-      // audio_samples_R[audio_write_cursor] = r
-      // audio_write_cursor = (audio_write_cursor + 1) & SAMPLE_MASK
+      _audio.freq = l || r
     },
   })
 
+  initController(nes)
+
   return {
     nes,
+    audio: _audio,
     mount(el: HTMLElement) {
-      el.appendChild(_canvas)
+      el.appendChild(_canvas.dom)
     },
     init() {
       ctx2d.fillStyle = 'black'
@@ -73,13 +73,9 @@ export function useNES() {
   }
 
   function render() {
-    if (ctx.paused) return
-
-    const cCtx = _canvas.getContext('2d')!
-
     const imageData = new ImageData(new Uint8ClampedArray(output.buf32.buffer), size.w, size.h)
 
-    cCtx.putImageData(imageData, 0, 0)
+    ctx2d.putImageData(imageData, 0, 0)
   }
 
   async function load(romPath: string) {
@@ -87,6 +83,7 @@ export function useNES() {
 
     nes.loadROM(romData)
 
+    _audio.start()
     run()
   }
 
@@ -95,6 +92,56 @@ export function useNES() {
     render()
 
     requestAnimationFrame(run)
+  }
+}
+
+function createCanvas() {
+  const canvas = document.createElement('canvas')
+  canvas.style.width = '100%'
+  canvas.width = size.w
+  canvas.height = size.h
+
+  const ctx2d = canvas.getContext('2d')!
+
+  return {
+    ctx2d,
+    dom: canvas,
+  }
+}
+
+function createAudioContext() {
+  const audioCtx = new AudioContext()
+
+  const mainGainNode = audioCtx.createGain()
+  mainGainNode.connect(audioCtx.destination)
+  mainGainNode.gain.value = 0.01
+
+  const osc = audioCtx.createOscillator()
+
+  osc.connect(mainGainNode)
+
+  osc.frequency.value = 0
+
+  return {
+    ctx: audioCtx,
+    start() {
+      osc.start()
+    },
+    stop() {
+      osc.stop()
+    },
+    set freq(v: number) {
+      osc.frequency.value = v
+    },
+    get freq() {
+      return osc.frequency.value
+    },
+    set volume(v: number) {
+      mainGainNode.gain.value = v
+    },
+    get volume() {
+      return mainGainNode.gain.value
+    },
   }
 }
 
@@ -109,4 +156,31 @@ async function loadRom(romPath: string) {
     reader.onerror = (e) => reject(e)
     reader.readAsBinaryString(res)
   })
+}
+
+function initController(nes: NES) {
+  const keyMap: Record<string, number> = {
+    w: Controller.BUTTON_UP,
+    s: Controller.BUTTON_DOWN,
+    a: Controller.BUTTON_LEFT,
+    d: Controller.BUTTON_RIGHT,
+
+    j: Controller.BUTTON_A,
+    k: Controller.BUTTON_B,
+    g: Controller.BUTTON_SELECT,
+    h: Controller.BUTTON_START,
+  }
+
+  useEventListener('keydown', (e) => keyboard(e, nes.buttonDown))
+
+  useEventListener('keyup', (e) => keyboard(e, nes.buttonUp))
+
+  function keyboard(e: KeyboardEvent, fn: any) {
+    const k = keyMap[e.key]
+    const player = 1
+
+    if (k != null) {
+      fn(player, k)
+    }
+  }
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,23 +5,35 @@ const output = ref<HTMLElement>()
 
 const ns = useNES()
 
+const freq = ref('')
+
+useRafFn(
+  () => {
+    freq.value = ns.audio.freq.toString()
+  },
+  {
+    immediate: true,
+  },
+)
+
 onMounted(async () => {
   ns.mount(output.value!)
 
-  loadRom()
+  // loadRom()
 })
 
 async function loadRom() {
   if (!output.value) return
 
   ns.init()
-  await ns.load('./roms/InterglacticTransmissing.nes')
+  await ns.load('./roms/Super Mario Bros.nes')
 }
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center w-screen h-screen gap-2">
-    <div ref="output"></div>
+    <div class="info">freq: {{ freq }}</div>
+    <div ref="output" class="aspect-[256/240] w-600px"></div>
     <NButton @click="loadRom">Load ROM</NButton>
   </div>
 </template>
